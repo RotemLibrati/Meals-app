a399a577e7a8666edd8f88c50d4fa7613975826d
RotemLibrati <rotemlibrati1@gmail.com>
2021-09-12 21:07:39 +0300

    add screens for nagigations

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
+
 const CategoriesScreen = props => {
     return (
-        <View>
-            <Text>Welcome</Text>
+        <View style={styles.container} >
+            <Text>Category Screen</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
 });
 
 export default CategoriesScreen;
