4dafb32bdd55af4cd90399714b186b9c9989ef2c
RotemLibrati <rotemlibrati1@gmail.com>
2021-09-12 18:32:08 +0300

    Added screens

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const CategoriesScreen = props => {
+    return (
+        <View>
+            <Text>Welcome</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default CategoriesScreen;
---
+++ screens/FiltersScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const FiltersScreen = props => {
+    return (
+        <View>
+            <Text>Welcome</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default FiltersScreen;
